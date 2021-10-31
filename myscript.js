6eb182c4b8bfec3a8e3e1c5a464a1129cb11aefa
ameen <ameenahmed1998@gmail.com>
2021-10-31 12:09:47 +0000

    finished basic prototype

--- myscript.js
+++ myscript.js
@@ -1,18 +1,37 @@
+const container = document.querySelector('.container')
+
+
 
 function populateBoard(num = 16) {
-    for (let i= 0; i < num; i++) {
-        box = createBox()
+    const container = document.querySelector('.container');
+    width = container.offsetWidth
+    height = container.offsetHeight
+    size = Number(width / num)
+    for (let i= 0; i < num**2; i++) {
+        box = createBox(size)
+
         container.appendChild(box)
-        console.log('printed box')
     }
 
 }
 
-function createBox() {
-    const container = document.querySelector('#container');
+function createBox(size) {
     const box = document.createElement('div');
-    box.classList.add('box');  
+    box.setAttribute('id','box');  
+
+    box.style.height = parseFloat(size) + "px";
+    box.style.width = parseFloat(size) + "px";
+
     return box  
 }
 
-populateBoard(16)
+function updateCell() {
+    this.style.backgroundColor = `${color.value}`
+}
+
+populateBoard(32)
+
+
+const boxes = container.querySelectorAll('#box');
+
+boxes.forEach(box => box.addEventListener('mouseover', updateCell))
